283bb8b1e49f27149ce1ea9ca243b197b56a82c5
Rajnish <b421037@iiit-bh.ac.in>
2023-07-12 22:05:49 +0530

    prime no prog

---
+++ isPrime.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+        if (n == 2)
+                return true;
+        else
+        {
+                for (int i = 2; i <= (n - 1); i++)
+                {
+                        if (n % i == 0)
+                        {
+                                return false;
+                        }
+                        else
+                        {
+                                return true;
+                        }
+                }
+        }
+}
+
+int main()
+{
+        int n;
+        cin >> n;
+        if (isPrime(n))
+        {
+                cout << "Prime";
+        }
+        else
+        {
+                cout << "Non-Prime";
+        }
+}
