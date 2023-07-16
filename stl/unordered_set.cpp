a34be4fc7cc93b281a0f230554ebcaaac3b627da
Rajnish <b421037@iiit-bh.ac.in>
2023-07-16 23:18:43 +0530

    unordered set in stl

---
+++ stl/unordered_set.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        // O(1)
+        // in worst case, it can go upto: O(n)
+        unordered_set<int> us;
+        // set will have unique values but order can be random
+        // all operations are similar to set but lower_bound and upper_bound do not work.
+        
+}
