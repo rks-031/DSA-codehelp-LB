aab2dcd8e0ed10ec26b7cac5e9c92aff058fb139
Rajnish <b421037@iiit-bh.ac.in>
2023-07-16 22:20:02 +0530

    queues in stl

---
+++ stl/queue.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        queue<int> q;
+        q.push(1);    //{1}
+        q.push(2);    //{1,2}
+        q.emplace(4); //{1,2,4}
+
+        q.back() += 5; //{1,2,9}
+
+        cout << q.front(); // prints 1
+
+        q.pop(); //{2,9}
+
+        cout << q.front(); // prints 2
+
+        // size, swap, empty same as stack
+}
