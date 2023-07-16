a37e8dc76180915cab1618e4749e98a719a22f96
Rajnish <b421037@iiit-bh.ac.in>
2023-07-16 22:03:45 +0530

    deque in stl

---
+++ stl/deque.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        deque<int> dq;
+        dq.push_back(1);     //{1}
+        dq.emplace_back(2);  //{1,2}
+        dq.push_front(4);    //{4,1,2}
+        dq.emplace_front(3); //{3,4,1,2}
+
+        dq.pop_back();  //{3,4,1}
+        dq.pop_front(); //{4,1}
+
+        dq.back();
+        dq.front();
+        // rest all functions are same as vector
+}
