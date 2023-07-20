5c6beda2e2a7e841103e5ad5a4d6356a973a6eed
Rajnish <b421037@iiit-bh.ac.in>
2023-07-20 21:45:16 +0530

    bubble sort

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int> &arr, int n)
+{
+        for (int i = 1; i < n; i++)
+        {
+                for (int j = 0; j < n - i; j++)
+                {
+                        if (arr[j] > arr[j + 1])
+                        {
+                                swap(arr[j], arr[j + 1]);
+                        }
+                }
+        }
+}
+
+int main()
+{
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+        {
+                cin >> arr[i];
+        }
+        cout << "Sorted array: " << endl;
+        bubbleSort(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+                cout << arr[i] << " ";
+        }
+}
