6585d739036cf19dbb357e1e44246f64e8a50af8
Rajnish <b421037@iiit-bh.ac.in>
2023-07-21 22:31:50 +0530

    length of the string

---
+++ strings/length_of_string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        char name[] = "Babbar";
+        int length = 0;
+        for (int i = 0; name[i] != '\0'; i++)
+        {
+                length++;
+        }
+        cout << length;
+}
