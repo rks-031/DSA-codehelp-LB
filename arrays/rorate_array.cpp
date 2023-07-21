6d8d1f79e5a721679c5a797de4dafec1f428e44d
Rajnish <b421037@iiit-bh.ac.in>
2023-07-21 14:43:25 +0530

    rotating array elements by k places

---
+++ arrays/rorate_array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<int> &nums, int k)
+{
+        vector<int> temp(nums.size());
+        for (int i = 0; i < nums.size(); i++)
+        {
+                int j = ((i + k) % nums.size());
+                temp[j] = nums[i];
+        }
+        nums = temp;
+}
+
+int main()
+{
+        int n, k;
+        cin >> n >> k;
+        vector<int> nums(n);
+        for (int i = 0; i < n; i++)
+        {
+                cin >> n;
+        }
+        rotate(nums, k);
+        cout << "Updated array after rotating the elements to " << k << " places:" << endl;
+        for (int i = 0; i < n; i++)
+        {
+                cout << nums[i] << " ";
+        }
+}
