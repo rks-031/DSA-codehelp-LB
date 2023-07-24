25607f884c0c402af8db6244e874d5c67171944d
Rajnish <b421037@iiit-bh.ac.in>
2023-07-24 12:22:42 +0530

    updates remove adjacent to satisfy all test cases

--- strings/remove_adjacent_duplicates.cpp
+++ strings/remove_adjacent_duplicates.cpp
@@ -4,12 +4,15 @@ using namespace std;
 string removeDuplicates(string &s)
 {
         int i = 0;
-        while (i < s.length())
+        while ((i < s.length() - 1) && (s.length() != 0))
         {
                 if (s[i] == s[i + 1])
                 {
-                        s.erase(i, 2);//(starting pos, count)
-                        i = 0;
+                        s.erase(i, 2); //(starting pos, count)
+                        if (i != 0)
+                        {
+                                i--;
+                        }
                 }
                 else
                 {
