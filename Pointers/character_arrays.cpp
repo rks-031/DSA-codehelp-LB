e37f0b1e672e0e675d47ff87d36690a681d4d7ea
Rajnish <b421037@iiit-bh.ac.in>
2023-07-25 13:48:10 +0530

    character arrays and pointers

---
+++ Pointers/character_arrays.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        int arr[5] = {1, 2, 3, 4, 5};
+        char ch[6] = "abcde"; // one extra space for null character
+        // cout << arr << endl;  // prints address
+        // cout << ch << endl;   // prints array
+
+        // now for pointers it's same
+        // int *p = &arr[0];
+        // cout << p << endl;
+        // char *c = &ch[0];
+        // cout << c << endl;
+
+        // char temp = 'z';
+        // char *p = &temp;
+        // cout << p << endl; // prints z and then some random values until p finds a null character
+
+        // char *c ='abcde';//never do this
+}
