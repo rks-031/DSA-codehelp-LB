9cb26652cf0bf60f46911c1573c88b54779a7fbb
Rajnish <b421037@iiit-bh.ac.in>
2023-07-25 13:13:14 +0530

    pointer array

---
+++ Pointers/pointer_array.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+        int arr[10] = {23, 122, 41, 67};
+        cout << arr << endl;
+        cout << arr[0] << endl;
+        cout << &arr[0] << endl;
+        cout << *arr << endl;
+        cout << *(arr + 2) << endl;
+        cout << 2 [arr] << endl;
+        cout << *arr + 1 << endl;
+        // arr[i] = *(arr+i) = *(i+arr) = i[arr]
+        
+}
