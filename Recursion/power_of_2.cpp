4960d8901794abc02fd33dd8c0da15464b1e1a9b
Rajnish <b421037@iiit-bh.ac.in>
2023-07-26 12:02:51 +0530

    power of two

---
+++ Recursion/power_of_2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int powerOfTwo(int n)
+{
+        if (n == 0)
+        {
+                return 1;
+        }
+        else
+        {
+                return 2 * powerOfTwo(n - 1);
+        }
+}
+
+int main()
+{
+        int n;
+        cin >> n;
+        cout << powerOfTwo(n) << endl;
+}
