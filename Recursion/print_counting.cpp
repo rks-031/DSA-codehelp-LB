ffe8f434610e1a37c76b4ad788f5eaabff1be812
Rajnish <b421037@iiit-bh.ac.in>
2023-07-26 12:11:34 +0530

    print counting

---
+++ Recursion/print_counting.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printCounting(int n)
+{
+        if (n == 0)
+        {
+                return;
+        }
+        cout << n << " ";
+        printCounting(n - 1);
+}
+
+int main()
+{
+        int n;
+        cin >> n;
+        printCounting(n);
+}
