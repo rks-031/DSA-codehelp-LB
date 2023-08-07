e4342c9cbebc92a18663b98a9ebe6f6d4aabe481
Rajnish <b421037@iiit-bh.ac.in>
2023-08-07 21:56:41 +0530

    Create cycle_detection.cpp

---
+++ Linked List/cycle_detection.cpp
@@ -0,0 +1,49 @@
+bool detectLoop(Node *head)
+{
+        if (head == NULL)
+        {
+                return false;
+        }
+
+        map<Node *, bool> visited;
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+                if (visited[temp] == true) // cycle is present
+                {
+                        return true;
+                }
+                visited[temp] = true;
+                temp = temp->next;
+        }
+        return false;
+}
+
+// floyd's cycle detection algorithm
+bool detectLoop(Node *head)
+{
+        if (head == NULL)
+        {
+                return false;
+        }
+
+        Node *slow = NULL;
+        Node *fast = NULL;
+
+        while (slow != NULL && fast != NULL)
+        {
+                fast = fast->nextl while (fast != NULL)
+                {
+                        fast = fast->next;
+                }
+
+                slow = slow->next;
+                if (slow == fast)
+                {
+                        return true;
+                }
+        }
+
+        return false;
+}
