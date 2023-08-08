4bcfc930a44a0374480e4ccf8521cc7c728cc1ab
Rajnish <b421037@iiit-bh.ac.in>
2023-08-08 16:15:21 +0530

    Create remove_duplicates_from_sorted.cpp

---
+++ Linked List/remove_duplicates_from_sorted.cpp
@@ -0,0 +1,23 @@
+ListNode *deleteDuplicates(ListNode *head)
+{
+        if (head == NULL)
+        {
+                return NULL;
+        }
+        ListNode *curr = head;
+        while (curr != NULL)
+        {
+                if ((curr->next != NULL) && (curr->val == curr->next->val))
+                {
+                        ListNode *next_next = curr->next->next;
+                        ListNode *nodeToDelete = curr->next;
+                        delete nodeToDelete;
+                        curr->next = next_next;
+                }
+                else
+                {
+                        curr = curr->next;
+                }
+        }
+        return head;
+}
