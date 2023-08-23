c29275b0eb5aab549c85f1aa856f96dfb5b4e860
Rajnish <b421037@iiit-bh.ac.in>
2023-08-23 22:09:38 +0530

    Create Reverse First K elements of Queue.cpp

---
+++ queue/Reverse First K elements of Queue.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+queue<int> modifyQueue(queue<int> q, int k);
+int main()
+{
+        int t;
+        cin >> t;
+        while (t-- > 0)
+        {
+                int n, k;
+                cin >> n >> k;
+                queue<int> q;
+                while (n-- > 0)
+                {
+                        int a;
+                        cin >> a;
+                        q.push(a);
+                }
+                queue<int> ans = modifyQueue(q, k);
+                while (!ans.empty())
+                {
+                        int a = ans.front();
+                        ans.pop();
+                        cout << a << " ";
+                }
+                cout << endl;
+        }
+}
+// } Driver Code Ends
+
+// User function Template for C++
+
+// Function to reverse first k elements of a queue.
+queue<int> modifyQueue(queue<int> q, int k)
+{
+        vector<int> ans;
+
+        while (q.size() != 0)
+        {
+                int temp = q.front();
+                q.pop();
+                ans.push_back(temp);
+        }
+
+        int start = 0;
+        int end = k - 1;
+        while (start <= end)
+        {
+                swap(ans[start++], ans[end--]);
+        }
+
+        for (int i = 0; i < ans.size(); i++)
+        {
+                q.push(ans[i]);
+        }
+
+        return q;
+}
