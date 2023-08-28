bbab45d40efd0c534b3d965774d9bd15b42b5e0f
Rajnish <b421037@iiit-bh.ac.in>
2023-08-28 09:14:03 +0530

    Create countLeafNodes..cpp

---
+++ Binary Trees/countLeafNodes..cpp
@@ -0,0 +1,45 @@
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+
+***********************************************************/
+void inorder(BinaryTreeNode<int> *root, int &count)
+{
+        // base case
+        if (root == NULL)
+        {
+                return;
+        }
+
+        inorder(root->left, count);
+        // cout << root-> data << " ";
+        if (root->left == NULL && root->right == NULL)
+        {
+                count++;
+        }
+        inorder(root->right, count);
+}
+
+int noOfLeafNodes(BinaryTreeNode<int> *root)
+{
+        // Write your code here.
+        int cnt = 0;
+        // leaf node: no children or simply no left child
+        inorder(root, cnt);
+
+        return cnt;
+}
