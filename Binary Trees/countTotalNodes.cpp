d3f6feda9eb9927345f68aefe573a62bb41d57d7
Rajnish <b421037@iiit-bh.ac.in>
2023-08-28 09:13:30 +0530

    introduction, traversals and count Total Nodes

---
+++ Binary Trees/countTotalNodes.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+private:
+        void countNodes(TreeNode *root, int &count)
+        {
+                if (root == NULL)
+                {
+                        return;
+                }
+
+                int l = countNodes(root->left);
+                int r = countNodes(root->right);
+                count = 1 + l + r;
+        }
+
+public:
+        int countNodes(TreeNode *root)
+        {
+                int count = 0;
+                countNodes(root, count);
+
+                return count;
+        }
+};
