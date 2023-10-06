cdab91bf9e65ee81b8dc5241fea3ede8270faf58
Rajnish <b421037@iiit-bh.ac.in>
2023-10-06 23:40:46 +0530

    Update pred_and_succ.cpp

--- Binary Search Tree/pred_and_succ.cpp
+++ Binary Search Tree/pred_and_succ.cpp
@@ -8,52 +8,47 @@
         TreeNode *left, *right;
         TreeNode() : data(0), left(NULL), right(NULL) {}
         TreeNode(int x) : data(x), left(NULL), right(NULL) {}
-        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left),
+right(right) {}
     };
 
 *************************************************************/
 
-pair<int, int> predecessorSuccessor(TreeNode *root, int key)
-{
-        // pred: left sub tree ka max value
-        // succ: right sub tree ka max value
+pair<int, int> predecessorSuccessor(TreeNode *root, int key) {
+  // Write your code here.
+  int pred = -1, succ = -1;
 
-        int pred = -1, succ = -1;
+  TreeNode *temp = root;
 
-        TreeNode *temp = root;
+  // finding key
+  while (temp != nullptr && temp->data != key) {
+    if (temp != nullptr && temp->data > key) {
+      succ = temp->data;
+      temp = temp->left;
+    }
+    if (temp != nullptr && temp->data < key) {
+      pred = temp->data;
+      temp = temp->right;
+    }
+  }
+  if (temp == nullptr) {
+    pair<int, int> result = make_pair(pred, succ);
+    return result;
+  }
+  // pred and succ
+  TreeNode *leftTree = temp->left;
+  while (leftTree != nullptr) {
+    pred = leftTree->data;
+    leftTree = leftTree->right;
+  }
 
-        // finding key
-        while (temp->data != key)
-        {
-                if (temp->data > key)
-                {
-                        succ = temp->data;
-                        temp = temp->left;
-                }
+  TreeNode *rightTree = temp->right;
+  while (rightTree != nullptr) {
+    succ = rightTree->data;
+    rightTree = rightTree->left;
+  }
 
-                if (temp->data < key)
-                {
-                        pred = temp->data;
-                        temp = temp->right;
-                }
-        }
+  pair<int, int> result = make_pair(pred, succ);
 
-        // pred and succ
-        TreeNode *leftTree = temp->left;
-        while (leftTree != nullptr)
-        {
-                pred = leftTree->data;
-                leftTree = leftTree->right;
-        }
-
-        TreeNode *rightTree = temp->right;
-        while (rightTree != nullptr)
-        {
-                succ = rightTree->data;
-                rightTree = rightTree->left;
-        }
-
-        pair<int, int> result = make_pair(pred, succ);
-
-        return result;
+  return result;
 }
