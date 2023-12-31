b9460ad0ce2ea6160ab9dff0e0aa88f031a0c6b8
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2023-12-31 22:00:12 +0530

    Create max occuring char.cpp

---
+++ strings/max occuring char.cpp
@@ -0,0 +1,30 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string str = "abcbdeaaf";
+    
+    unordered_map<char, int> freq;
+    
+    for(int i=0;i<str.length();i++){
+        freq[str[i]]++;
+    }
+    
+    int maxi = INT_MIN;
+    
+    auto it = freq.begin();
+    
+    while(it != freq.end()){
+        maxi = max(maxi, it->second); it++;
+    }
+    
+    it = freq.begin();
+    
+    while(it!=freq.end()){
+        if(it->second == maxi) cout<<it->first; 
+        it++;
+    }
+    
+
+    return 0;
+}
