b45faabcea284aa8d5b026b06c7cd3df3aa0d844
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-01 11:08:49 +0530

    Update kadane_algo.cpp

--- arrays/kadane_algo.cpp
+++ arrays/kadane_algo.cpp
@@ -5,8 +5,7 @@ using namespace std;
 long long maxSubarraySum(vector<int> arr, int n)
 {
     long long currentSum = 0;
-    long long maxSum = LLONG_MIN; // Use LLONG_MIN instead of INT_MIN for long long
-
+    long long maxSum = LLONG_MIN;
     for (int i = 0; i < n; i++)
     {
         currentSum = max((long long)arr[i], currentSum + arr[i]);
