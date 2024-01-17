f12fb79d5aa57d33be2052aed68ba8301ca360d4
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-17 13:01:13 +0530

    Kruskal's algo implemented

---
+++ Graphs/Kruskal algo.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+bool cmp(vector<int> &a, vector<int> &b)
+{
+    return a[2] < b[2];
+}
+
+void makeSet(vector<int> &parent, vector<int> &rank, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+int findParent(vector<int> &parent, int node)
+{
+    if (parent[node] == node)
+    {
+        return node;
+    }
+    return parent[node] = findParent(parent, parent[node]);
+}
+
+void unionSet(int u, int v, vector<int> &parent, vector<int> &rank)
+{
+    u = findParent(parent, u);
+    v = findParent(parent, v);
+
+    if (rank[u] < rank[v])
+    {
+        parent[u] = v;
+    }
+    else if (rank[v] < rank[u])
+    {
+        parent[v] = u;
+    }
+    else
+    {
+        parent[v] = u;
+        rank[v]++;
+    }
+}
+
+int minimumSpanningTree(vector<vector<int>> &edges, int n)
+{
+    sort(edges.begin(), edges.end(), cmp);
+
+    vector<int> parent(n);
+    vector<int> rank(n);
+
+    int minWt = 0;
+
+    makeSet(parent, rank, n);
+
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = findParent(parent, edges[i][0]);
+        int v = findParent(parent, edges[i][1]);
+        int w = edges[i][2];
+
+        if (u != v)
+        {
+            minWt += w;
+            unionSet(u, v, parent, rank);
+        }
+    }
+
+    return minWt;
+}
