9af77f76ed88650293f245fcc6616ad2ee759423
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-25 21:41:09 +0530

    floyd warshall algo

---
+++ Graphs/Floyd Warshall.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    void shortest_distance(vector<vector<int>> &matrix)
+    {
+        // Code here
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == -1)
+                    matrix[i][j] = 1e9;
+
+                if (i == j)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        // algorithm
+        for (int k = 0; k < n; k++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 1e9)
+                    matrix[i][j] = -1;
+
+                if (i == j)
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+};
