97d86a09529ec4f037f437e2faa0c7c50c7f3180
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-27 00:18:57 +0530

    fibo number using bottom up approach

---
+++ Dynamic programming/Fibonacci-bottom-up.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibo(int n, vector<int> &dp)
+{
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1, -1);
+
+    int ans = fibo(n, dp);
+    cout << ans;
+}
