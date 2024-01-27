6d43e0f6c4d63829b6537ccd4f32ba38427cd122
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-27 19:00:38 +0530

    space optimization done

--- Dynamic programming/Minimum cost of climbing stairs.cpp
+++ Dynamic programming/Minimum cost of climbing stairs.cpp
@@ -13,19 +13,35 @@ private:
     // }
 
     // this function follows bottom up approach: tabulation
+    // int solve(int n, vector<int> &cost)
+    // {
+    //     vector<int> dp(n + 1, -1);
+
+    //     dp[0] = cost[0];
+    //     dp[1] = cost[1];
+
+    //     for (int i = 2; i < n; i++)
+    //     {
+    //         dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+    //     }
+
+    //     return min(dp[n - 1], dp[n - 2]);
+    // }
+
+    // space optimization: O(1)
     int solve(int n, vector<int> &cost)
     {
-        vector<int> dp(n + 1, -1);
-
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        int prev2 = cost[0];
+        int prev1 = cost[1];
 
         for (int i = 2; i < n; i++)
         {
-            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+            int curr = cost[i] + prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return min(dp[n - 1], dp[n - 2]);
+        return min(prev1, prev2);
     }
 
 public:
