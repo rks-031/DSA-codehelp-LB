85886c28576b8ae84f1aaba2d8c47a047da05fc7
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-27 18:53:45 +0530

    minimum cost of climbing stairs

--- Dynamic programming/Minimum cost of climbing stairs.cpp
+++ Dynamic programming/Minimum cost of climbing stairs.cpp
@@ -1,9 +1,10 @@
 class Solution
 {
 private:
-    // int solve(vector<int> &cost, int n, vector<int>&dp){
-    //     if(n==0) return cost[0];
-    //     if(n==1) return cost[1];
+    // this function follows top down approach: recursion + memoization
+    //  int solve(vector<int> &cost, int n, vector<int>&dp){
+    //      if(n==0) return cost[0];
+    //      if(n==1) return cost[1];
 
     //     if(dp[n]!=-1) return dp[n];
 
@@ -11,6 +12,7 @@ private:
     //     return dp[n];
     // }
 
+    // this function follows bottom up approach: tabulation
     int solve(int n, vector<int> &cost)
     {
         vector<int> dp(n + 1, -1);
