a659f887b4d2ddcae063ab114670ad719a5f3831
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-31 17:47:11 +0530

    Create Painting Fence Algotrithm.cpp

---
+++ Dynamic programming/Painting Fence Algotrithm.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+
+int add(int a, int b)
+{
+    return (a % MOD + b % MOD) % MOD;
+}
+
+int mul(int a, int b)
+{
+    return (a % MOD * 1LL * b % MOD) % MOD;
+}
+
+// int solve(int n, int k, vector<int>&dp){
+//     if(n==1) return k;
+
+//     if(n==2) return k+k*(k-1);
+
+//     if(dp[n]!=-1) return dp[n];
+
+//     int ans = add(mul(solve(n-2,k,dp),k-1), mul(solve(n-1,k,dp),k-1));
+
+//     return dp[n] = ans;
+// }
+
+// int solve(int n, int k){
+//     vector<int>dp(n+1,0);
+
+//     dp[1] = k;
+//     dp[2] = add(k,mul(k,k-1));
+
+//     for(int i=3;i<=n;i++){
+//         dp[i] = add(mul(dp[i-2],k-1) , mul(dp[i-1],k-1));
+//     }
+//     return dp[n];
+// }
+
+int solve(int n, int k)
+{
+    int prev2 = k;
+    int prev1 = add(k, mul(k, k - 1));
+
+    for (int i = 3; i <= n; i++)
+    {
+        int ans = add(mul(prev2, k - 1), mul(prev1, k - 1));
+        prev2 = prev1;
+        prev1 = ans;
+    }
+
+    return prev1;
+}
+
+int numberOfWays(int n, int k)
+{
+    // vector<int> dp(n+1,-1);
+    // return solve(n,k,dp);.
+    return solve(n, k);
+}
