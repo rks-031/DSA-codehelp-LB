3ad306ea35b976b2db7d3f59d4bf11679f316e3b
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-02-05 14:02:54 +0530

    Update Minimum Cost of Tickets.cpp

--- Dynamic programming/Minimum Cost of Tickets.cpp
+++ Dynamic programming/Minimum Cost of Tickets.cpp
@@ -1,37 +1,38 @@
-#include <bits/stdc++.h>
-
-int solve(int n, vector<int> days, vector<int> cost, int index, vector<int> &dp)
+class Solution
 {
-    // base case
-    if (index >= n)
+private:
+    int solve(int n, vector<int> &days, vector<int> &costs, vector<int> &dp, int index)
     {
-        return 0;
+        if (index >= n)
+        {
+            return 0;
+        }
+
+        if (dp[index] != -1)
+            return dp[index];
+
+        // day-1 pass
+        int op1 = costs[0] + solve(n, days, costs, dp, index + 1);
+
+        int i;
+        // day-7 pass
+        for (i = index; i < n && days[i] < days[index] + 7; i++)
+            ;
+        int op2 = costs[1] + solve(n, days, costs, dp, i);
+
+        // day-30 pass
+        for (i = index; i < n && days[i] < days[index] + 30; i++)
+            ;
+        int op3 = costs[2] + solve(n, days, costs, dp, i);
+
+        return dp[index] = min(op1, min(op2, op3));
     }
 
-    if (dp[index] != -1)
-        return dp[index];
-
-    // 1 day pass
-    int op1 = cost[0] + solve(n, days, cost, index + 1, dp);
-
-    // 7 days pass
-    int i;
-    for (i = index; i < n && days[i] < days[index] + 7; i++)
-        ;
-
-    int op2 = cost[1] + solve(n, days, cost, i, dp);
-
-    // 30 days pass
-    for (i = index; i < n && days[i] < days[index] + 30; i++)
-        ;
-
-    int op3 = cost[2] + solve(n, days, cost, i, dp);
-
-    return dp[index] = min(op1, min(op2, op3));
-}
-
-int minimumCoins(int n, vector<int> days, vector<int> cost)
-{
-    vector<int> dp(n + 1, -1);
-    return solve(n, days, cost, 0, dp);
-}
+public:
+    int mincostTickets(vector<int> &days, vector<int> &costs)
+    {
+        int n = days.size();
+        vector<int> dp(n + 1, -1);
+        return solve(n, days, costs, dp, 0);
+    }
+};
